9a0f93173ca15751b0c64afe1fdd20d7f21aef7d
bibakroy <roybibak1@gmail.com>
2023-05-07 17:52:40 +0600

    fix css module issue with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Register.module.css";
+
+function Register() {
+  return (
+    <div className={styles.container}>
+      <h1>Register</h1>
+    </div>
+  );
+}
+
+export default Register;
